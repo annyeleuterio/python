a0b89351186efc04bbd65d6a28042f550eab946d
annyeleuterio <anny.eleuterio@gmail.com>
2024-12-05 11:26:01 -0300

    variáveis delta

---
+++ aula02/aula2.py
@@ -0,0 +1,11 @@
+print('friends melhor serie do mundo')
+
+nome_usuario = 'barbie' # tipo -> str = string
+idade_usuario = 65 # tipo -> int = integer
+altura_usuario = 1.80 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool= boolean
+
+print('welcome', nome_usuario)
+print('your age is', idade_usuario, 'years.')
+print('you are', altura_usuario, 'meters tall.')
+print('are you of legal age?', maior_idade)
